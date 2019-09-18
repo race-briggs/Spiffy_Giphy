fbfd9d919363d6d65993c9386a487363cfdb0051
race-briggs <race.briggs@gmail.com>
2019-09-18 16:11:45 -0500

    added an if statement to prevent repeat buttons

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,25 +3,52 @@ $(document).ready(function(){
   var searchButtons = ['dog', 'cat', 'bird', 'cute', 'dumb', 'moose', 'skateboarding', 'snowboarding', 'mountains', 'nature'];
 
   $(document).on('click', '.search-btn', function(){
+
+    var searchQuery = $(this).text();
+
+    callGiphy(searchQuery);
+  });
+
+  function populateButtons(){
+    $('#button-container').empty();
+    searchButtons.forEach(function(button){
+      var newButton = $('<button class="search-btn">')
+      newButton.text(button);
+      $('#button-container').append(newButton);
+    })
+  }
+
+  $('.new-search-btn').on('click', function(){
+    var newValue = $('.gif-search').val();
+    searchButtons.forEach(function(value){
+      if(searchButtons.includes(newValue)){
+        callGiphy(newValue)
+      } else {
+        searchButtons.push(newValue);
+        callGiphy(newValue);
+      }
+    })
+    populateButtons();
+  })
+
+  function callGiphy(query){
     var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=NAKkHeBxebc7yukomDEoaFQUnJTPvrXd&";
 
-    console.log(this);
+    q = query
 
-    var query = $(this).text();
+    console.log(q);
 
-    console.log(query);
-
-    queryURL = queryURL + "limit=10&q=" + query;
+    queryURL = queryURL + "limit=10&q=" + q;
 
     console.log(queryURL)
 
     $.ajax({
       url: queryURL,
       method: "GET"
-    }).then(function(response){
+    }).then(function (response) {
       console.log(response);
       $('.gif-holder').empty();
-      response.data.forEach(function(gif){
+      response.data.forEach(function (gif) {
         var gifDiv = $('<div>');
         var rating = $('<p>');
         var gifImg = $('<img>');
@@ -35,22 +62,7 @@ $(document).ready(function(){
         $('.gif-holder').append(gifDiv);
       })
     })
-
-  });
-
-  function populateButtons(){
-    searchButtons.forEach(function(button){
-      var newButton = $('<button class="search-btn">')
-      newButton.text(button);
-      $('#button-container').append(newButton);
-    })
   }
-
-  $('.new-search-btn').on('click', function(){
-    var newValue = $('.gif-search').val();
-  })
-
-
 
   populateButtons()
 })
